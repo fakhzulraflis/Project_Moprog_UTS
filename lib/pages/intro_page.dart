4d424891610e968a7f72651418ac6298e47d4acd
fakhzulraflis <rfakhzul@gmail.com>
2026-09-04 22:00:00 +0700

    feat(ui): redesign intro screen

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -13,9 +14,16 @@ class IntroPage extends StatelessWidget {
               left: 80.0,
               right: 80,
               bottom: 0,
-              top: 200,
+              top: 146,
             ),
-            child: Image.asset('assets/app/2.gif'),
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Image.asset(
+                'assets/app/wavingduck.gif',
+                height: 170.0,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
 
           Padding(
@@ -23,18 +31,24 @@ class IntroPage extends StatelessWidget {
             child: Text(
               "Learn languages one Quack at a time.",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
+              style: GoogleFonts.baloo2(
+                fontSize: 25,
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 255, 255, 255),
               ),
             ),
           ),
 
-          Text(
-            "Quack — O serving you anywhere, anytime, for free.",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Color.fromARGB(172, 255, 255, 255)),
+          Padding(
+            padding: const EdgeInsets.only(top: 1.0, bottom: 1.0),
+            child: Text(
+              "Quack — O serving you anywhere, anytime, for free.",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.pixelifySans(
+                fontSize: 12.8,
+                color: Color.fromARGB(172, 255, 255, 255),
+              ),
+            ),
           ),
 
           Padding(
@@ -42,11 +56,11 @@ class IntroPage extends StatelessWidget {
               left: 10,
               right: 10,
               bottom: 0,
-              top: 30,
+              top: 31,
             ),
             child: Container(
               width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 16),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 30),
               decoration: BoxDecoration(
                 color: const Color.fromARGB(255, 231, 194, 73),
                 borderRadius: BorderRadius.circular(16),
@@ -59,13 +73,13 @@ class IntroPage extends StatelessWidget {
                   ),
                 ],
               ),
-              child: const Center(
+              child: Center(
                 child: Text(
                   "GET STARTED",
-                  style: TextStyle(
+                  style: GoogleFonts.baloo2(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
+                    fontSize: 20,
                     letterSpacing: 0.7,
                   ),
                 ),
@@ -94,10 +108,10 @@ class IntroPage extends StatelessWidget {
                   ),
                 ],
               ),
-              child: const Center(
+              child: Center(
                 child: Text(
                   "I ALREADY HAVE ACCOUNT",
-                  style: TextStyle(
+                  style: GoogleFonts.baloo2(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
