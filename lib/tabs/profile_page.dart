f080511acaddf7a1cc6af8ca9d193ec6f927aa90
fakhzulraflis <rfakhzul@gmail.com>
2026-09-24 22:04:22 +0700

    feat: add profile completion card

--- lib/tabs/profile_page.dart
+++ lib/tabs/profile_page.dart
@@ -34,6 +34,10 @@ class ProfilePage extends StatelessWidget {
                 const SizedBox(height: 30),
 
                 _buildEditButton(),
+
+                const SizedBox(height: 30),
+
+                _buildCompleteProfileCard(),
               ],
             ),
           ),
@@ -146,6 +150,91 @@ class ProfilePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCompleteProfileCard() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+
+      decoration: BoxDecoration(
+        color: const Color(0xFF20272B),
+        borderRadius: BorderRadius.circular(24),
+      ),
+
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Complete your profile!",
+                      style: GoogleFonts.baloo2(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    const SizedBox(height: 6),
+
+                    Text(
+                      "1 STEP LEFT",
+                      style: GoogleFonts.pixelifySans(
+                        color: Colors.white54,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Image.asset('assets/app/qua.gif', 
+              height: 100,
+              width: 80,
+              fit: BoxFit.cover,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 15),
+
+          Container(
+            width: double.infinity,
+
+            padding: const EdgeInsets.symmetric(vertical: 12),
+
+            decoration: BoxDecoration(
+              color: const Color(0xFF55B6E8),
+
+              borderRadius: BorderRadius.circular(16),
+
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0xFF3895C5),
+                  offset: Offset(0, 4),
+                  blurRadius: 0,
+                ),
+              ],
+            ),
+
+            child: Center(
+              child: Text(
+                "CONTINUE",
+                style: GoogleFonts.baloo2(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
